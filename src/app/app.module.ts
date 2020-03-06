2087af954448ebf3231e5937bc9764f59f826599
Yusuf Benjamin <mughammad.benjamin@ioco.tech>
2020-03-06 07:31:29 +0200

    --cleans: boilerplate code; --adds: layout component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App Modules
 import { AppRoutingModule } from './app-routing.module';
+
+// App Components
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
