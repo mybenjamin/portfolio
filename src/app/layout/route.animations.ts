731d82853fc8365c6c817552f8b191e698ebaf88
Yusuf Benjamin <mughammad.benjamin@ioco.tech>
2020-03-13 08:31:57 +0200

    --refactors: route animations;

--- src/app/layout/route.animations.ts
+++ src/app/layout/route.animations.ts
@@ -1,23 +1,33 @@
-import { trigger, transition, style, query, animateChild, group, animate } from '@angular/animations';
+import { trigger, transition, style, query, animateChild, group, animate, keyframes } from '@angular/animations';
 
 export const slideInAnimation =
   trigger('routeAnimations', [
     transition('* <=> *', [
+      style({ position: 'relative' }),
 
-      // Animate entering element
-      query(':enter', [
-        style({ opacity: 0 }),
-        animate('.5s', style({ opacity: 1 })),
-        animateChild()],
-        { optional: true }
-      ),
+      // Initial State
+      query(':enter, :leave', style({ position: 'absolute' }), { optional: true }),
 
-      // Animate the exiting element
-      // Can group to play animation in paralell
-      query(':leave', [
-        animate('.5s', style({ opacity: 0 })),
-        animateChild()],
-        { optional: true }
-      ),
-    ]),
+      // Queries can be grouped to play animation in paralell
+      group([
+
+        // Enter Initial Style
+        query(':enter', [style({ opacity: '0', transform: 'translateX(-150em)' })], { optional: true }),
+
+        // Enter Animation
+        query(':enter', [
+          animate('.8s', keyframes([
+            style({ opacity: 1, transform: 'translateX(0)' }),
+          ])),
+          animateChild()], { optional: true }),
+
+        // Leave Animation
+        query(':leave', [
+          animate('.8s', keyframes([
+            style({ transform: 'scale(0.9)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateX(150em)', offset: 1 }),
+          ])),
+          animateChild()], { optional: true }),
+      ])
+    ])
   ]);
